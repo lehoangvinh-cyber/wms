9469d79271eb0ab0056dba46eeffd43f68797468
lehoangvinh-cyber <85878817+lehoangvinh-cyber@users.noreply.github.com>
2026-05-27 10:39:32 +0700

    v3.9

--- inventory/urls.py
+++ inventory/urls.py
@@ -30,4 +30,17 @@ urlpatterns = [
     path('nhat-ky-he-thong/', views.audit_log, name='audit_log'),
     # Đảm bảo không có <int:debt_id> ở trong ngoặc đơn
     path('in-phieu-ban-giao/', views.print_receipt, name='print_receipt'),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
